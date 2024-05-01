4c1cc616aeca95be0eab38ffe7b9423c706be5ee
callum32 <callumh26@aol.com>
2024-05-01 16:55:28 +0100

    Fixed a few issues
    This reverts commit 4179812b82dd00484497db94a4978e9192801b29.

--- Assets/Scripts/PatternGeneration.cs
+++ Assets/Scripts/PatternGeneration.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PatternGeneration : MonoBehaviour
 {
+    public GameObject pattern;
+    private bool generate = true;
+    public int counter;
+    private int spawned;
+    private TrypophobiaWin Win;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnPattern", 0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (counter == 50)
+        {
+            generate = false;
+            Win.Win = true;
+        }
+        if (spawned == 100)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+
+    void SpawnPattern()
+    {
+        if (generate == true)
+        {
+            Vector3 RandomSpawnPosition = new Vector3(Random.Range(-7.65f, 7.7f), Random.Range(-0.62f, 0.63f), Random.Range(-7.65f, 7.7f));
+            Instantiate(pattern, RandomSpawnPosition, Quaternion.identity);
+            spawned++;
+        }
     }
 }
