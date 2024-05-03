1255fcd2921150484a12ab7a3688d07cbbd48a1b
RSTHunt <157506363+RSTHunt@users.noreply.github.com>
2024-05-03 15:14:24 +0100

    most level design done

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource SoundToPlay;
+    public BoxCollider HitDetector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HitDetector = this.GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (SoundToPlay != null)
+        {
+            SoundToPlay.Play();
+        }
+      
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (SoundToPlay != null)
+        {
+            SoundToPlay.Stop();
+        }
+    }
+}
